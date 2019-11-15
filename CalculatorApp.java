935fdd6bd5f8d1f56bb1e9a7689431ced0f1f7a6
Pappu Kumar <pappuray>
2019-11-15 18:30:13 +0530

    here we implemented all the methods

---
+++ CalculatorApp.java
@@ -0,0 +1,25 @@
+package com.sg.calc;
+
+public class CalculatorApp implements ICalculatorApp {
+
+	public int sum() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int sub() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int mul() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int div() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
